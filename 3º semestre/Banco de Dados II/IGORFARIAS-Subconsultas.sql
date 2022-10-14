0d11478c3e7ce4dc64edae90c407938d94a4f40c
Igor Farias <balestfarias@gmail.com>
2022-10-14 18:20:03 -0300

    subconsultas
    3~

--- 3º semestre/Banco de Dados II/IGORFARIAS-Subconsultas.sql
+++ 3º semestre/Banco de Dados II/IGORFARIAS-Subconsultas.sql
@@ -6,15 +6,35 @@
 -- Liste todos os nomes de chefe de setor (Chefe), o respectivo nome do setor (Setor), o salario (Salário) e o nome da empresa (Empresa), ordenados pelo salário (do maior para o menor).
 -- Envie o sql na página para conferência.
 
-SELECT * FROM funcionario WHERE  EXISTS (
-	SELECT setor.setor_id FROM setor WHERE chefe IS NULL AND setor.setor_id = funcionario.setor_id
-);
+USE bd2;
 
-SELECT * FROM funcionario JOIN setor on setor.setor_id = funcionario.funcionario_id AND setor.setor_id IS NULL;
+-- SELECT * FROM funcionario WHERE  EXISTS (
+-- 	SELECT setor.setor_id FROM setor WHERE chefe IS NULL AND setor.setor_id = funcionario.setor_id
+-- );
+
+-- SELECT * FROM funcionario JOIN setor on setor.setor_id = funcionario.funcionario_id AND setor.setor_id IS NULL;
 
 
-SELECT * FROM funcionario LEFT JOIN setor on setor.setor_id = funcionario.funcionario_id AND setor.setor_id IS NULL AND funcionario.setor_id IS NULL;
+-- SELECT * FROM funcionario LEFT JOIN setor on setor.setor_id = funcionario.funcionario_id AND setor.setor_id IS NULL AND funcionario.setor_id IS NULL;
 
+-- 1ª consulta
 (SELECT * FROM funcionario WHERE EXISTS(SELECT funcionario.setor_id WHERE funcionario.setor_id IS NULL)) 
 UNION 
 ( SELECT * FROM funcionario WHERE EXISTS(SELECT setor.setor_id FROM setor WHERE chefe IS NULL AND setor.setor_id = funcionario.setor_id));
+
+
+-- 2ª consulta
+-- com subconsulta
+SELECT nome as NomeDoFuncionario, (SELECT nome FROM setor WHERE funcionario.setor_id=setor.setor_id) as NomeDoSetor FROM funcionario WHERE setor_id=3 OR setor_id=4;
+
+-- sem subconsulta
+
+-- 3ª consulta
+SELECT nome as NomeDoFuncionario, 
+		salario,
+		(SELECT nome FROM setor WHERE funcionario.setor_id = setor.setor_id AND 
+									  setor_id IN (3,4) AND 
+                                      setor.nome IS NOT NULL) as NomeDoSetor 
+		FROM funcionario ORDER BY  NomeDoSetor, salario DESC;
+
+
