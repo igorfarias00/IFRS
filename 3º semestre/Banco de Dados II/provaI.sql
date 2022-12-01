3ae90f1cca7ecc4fc46767ad3081960f2b69804c
Igor Farias <balestfarias@gmail.com>
2022-11-30 21:39:51 -0300

    prova-progIII

--- 3º semestre/Banco de Dados II/provaI.sql
+++ 3º semestre/Banco de Dados II/provaI.sql
@@ -2,6 +2,12 @@
 -- Considerando o banco de dados (bd2) disponível no moodle e usado em aula, faça as seguintes consultas: 
 USE bd2;
 
+CREATE USER 'professor'@'localhost' IDENTIFIED BY 'roboto';
+GRANT SELECT,INSERT ON bd2.empresa TO 'professor'@'localhost';
+
+SHOW GRANTS FOR 'professor'@'localhost';
+DROP USER 'professor'@'localhost';
+
 -- 1 ** ---------------------------------------------------------------------------------------------------
 -- 		selecione todos os funcionários que tenham Salário menor que 1500 e que tenham um chefe com salário maior que 1500. 
 --  	Faça 2 consultas, uma com o uso de subconsultas e outra sem o uso de subconsultas.
@@ -23,6 +29,7 @@ SELECT funcionario.nome, funcionario.salario
     WHERE funcionario.salario < 1500 OR 
 		  ((SELECT setor.chefe FROM setor WHERE setor.chefe = funcionario.funcionario_id)  AND funcionario.salario < 1500)
 	ORDER BY salario DESC;
+    
 
 -- 2 ** ----------------------------------------------------------------------------------------------------
 -- Resolva as seguintes questões (cole o código SQL como texto e envie). 
@@ -40,7 +47,30 @@ SELECT empresa.nome, (	SELECT AVG(funcionario.salario)
                                 WHERE setor.empresa_id = empresa.empresa_id AND
 									  funcionario.sexo="masculino") AS média
 	 FROM empresa ;
+     
+     
+-- correção 
 
+SELECT empresa.nome, AVG(funcionario.salario) 
+	FROM empresa 
+	JOIN setor USING (empresa_id ) 
+    JOIN funcionario USING(setor_id) 
+    WHERE empresa.empresa_id !=2 AND 
+		  funcionario.sexo = "masculino"
+	GROUP BY empresa.nome;
+
+SELECT empresa.nome, (
+		SELECT AVG(funcionario.salario) FROM funcionario WHERE
+			funcionario.sexo = "masculino" AND
+            funcionario.setor AND
+            funcionario.setor_id IN (
+				SELECT setor.setor_id FROM setor WHERE
+                setor.setor_id = funcionario.setor_id AND
+                setor.empresa_id = empresa.empresa_id AND 
+                setor.empresa_id is not NULL
+            )
+		) FROM empresa WHERE empresa.empresa != 2;
+            
 -- 3 ** --------------------------------------------------------------------------------------------------------
 -- Selecione os funcionários que não são chefes e que estão em setor de andar 0 que ganham entre 1300 e 1500, 
 -- ordenando pela ordem alfabética.  Resolva a questão e entregue as strings SQL sem e com subconsultas.
@@ -57,12 +87,26 @@ SELECT funcionario.nome, funcionario.salario
 SELECT * FROM setor;
 
 
+
 -- c/ subconsultas
 
-SELECT funcionario.nome, funcionario.salario FROM funcionario WHERE funcionario.funcionario_id != ( SELECT setor.chefe FROM setor WHERE setor.setor_id = funcionario.setor_id) AND 
-											   0 = (SELECT setor.andar FROM setor WHERE setor.setor_id = funcionario.setor_id) AND 
-                                               funcionario.salario >= 1300 AND funcionario.salario <= 1500
-		ORDER BY funcionario.nome;
+SELECT funcionario.nome, funcionario.salario 
+	FROM funcionario 
+    WHERE funcionario.funcionario_id != ( SELECT setor.chefe FROM setor WHERE setor.setor_id = funcionario.setor_id) AND 
+		  0 = (SELECT setor.andar FROM setor WHERE setor.setor_id = funcionario.setor_id) AND 
+          funcionario.salario >= 1300 AND funcionario.salario <= 1500
+	ORDER BY funcionario.nome;
+    
+-- correção
+SELECT funcionario.nome 
+	FROM funcionario 
+    JOIN setor 
+    ON funcionario.funcionario_id != setor.chefe AND 
+	   setor.chefe is not NULL and setor.andar = 0 AND 
+	   funcionario.salario > 1300 and funcionario.salario < 1500 ORDER by funcionario.nome;
+
+-- SELECT funcionario.nome FROM funcionario WHERE funcionario.salario > 1300 AND funcionario.salario < 1500 AND funcionario.funcionario_id NOT IN(
+-- 	SELECT chefe FROM setor WHERE chefe;
 -- ----------------------------
 -- 4 **
 -- Crie uma visão (VIEW) para o banco db2 que contenha o nome da empresa, e nome dos setor que possuam média salarial maior que 1200.
@@ -104,6 +148,7 @@ DROP VIEW questao5;
 
 SELECT * FROM questao5 ORDER BY ANDAR;
 
-UPDATE questao5 SET ANDAR= ANDAR+1 WHERE ANDAR != 0;
+UPDATE questao5 SET ANDAR = ANDAR+1 WHERE ANDAR != 0;
+
 SELECT * FROM questao5 ORDER BY ANDAR;
 SELECT setor.nome as NOME, setor.andar as ANDAR, setor.setor_id as ID FROM setor ORDER BY ANDAR;
