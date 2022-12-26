ac26b78a6a54fa926f45a7c6ca965f0db63a2936
Igor Farias <balestfarias@gmail.com>
2022-12-26 11:49:18 -0300

    job

---
+++ 3º semestre/Sistemas Operacionais/threads/src/threads/Main.java
@@ -0,0 +1,26 @@
+package threads;
+
+import java.lang.Math;
+
+public class Main {
+	
+	
+	public static void main(String[] args){
+		double interacoes = 6;
+
+		
+		double e = Math.pow((1+ (1 / interacoes)), interacoes);
+		
+		System.out.println(e);
+		
+
+		
+
+	}
+	
+
+}
+
+
+
+
